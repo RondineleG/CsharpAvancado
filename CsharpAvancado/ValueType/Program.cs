f62fa86c28767e289a10197e637baa80a2fad965
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-05-27 10:33:57 -0300

    Value Type

---
+++ CsharpAvancado/ValueType/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ValueType
+{
+    class Program
+    {
+        /*
+         Quando você passa uma variável
+         de tipo de valor de um método 
+         para outro método, o sistema
+         cria uma cópia separada de uma
+         variável em outro método, de
+         forma que se o valor for alterado
+         no método não afetará
+         a variável em outro método.
+         */
+        static void MudarValor(int x)
+        {
+            x = 200;
+            Console.WriteLine(x);
+        }
+
+        public static void Main()
+        {
+            int i = 100;
+            Console.WriteLine(i);
+            MudarValor(i);
+            Console.WriteLine(i);
+            Console.ReadLine();
+
+            /*
+             a variável ino método Main () permanece inalterada
+             mesmo após passá-la para o 
+             método MudarValor() e alterar seu valor lá.
+             */
+        }
+    }
+}
