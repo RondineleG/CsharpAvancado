73e39ec1805daf2a0febebb8a3a77b54d36a66e8
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-06-12 16:39:49 -0300

    Type Inference e Dynamic

---
+++ CsharpAvancado/TypeInference_Dynamic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TypeInference_Dynamic
+{
+    public class Connection
+    {
+        public string ConnectionString;
+
+        public void Conectar()
+        {
+            Console.WriteLine("Conectando...");
+        }
+        public void Fechar()
+        {
+            Console.WriteLine("Fechando...");
+        }
+    }
+
+    public static class Fabrica
+    {
+        public static Connection CriarConnnection()
+        {
+            return new Connection();
+        }
+    }
+    class Program
+    {  
+
+        static void Main(string[] args)
+        {
+            var teste = "TESTE";
+            var numero = 10;
+            dynamic v = "TESTE";
+            var con = Fabrica.CriarConnnection();
+            con.ConnectionString = "BANCO";
+            con.Conectar();
+            con.Fechar();
+            Console.ReadLine();
+        }
+    }
+}
