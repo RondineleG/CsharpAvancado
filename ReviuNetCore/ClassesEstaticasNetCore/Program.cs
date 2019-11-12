488ded32affd18713f9a57b2f79a6d63d52cebf6
Rondinele <rondineleg@edu.univali.br>
2019-11-12 11:28:44 -0300

    recriando projetos com Netcore

---
+++ ReviuNetCore/ClassesEstaticasNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassesEstaticasNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando Projetos NEtCore");
+        }
+    }
+}
