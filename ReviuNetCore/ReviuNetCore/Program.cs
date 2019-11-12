b63f3170a04b40e2a022dffa352a90a8ccdc7ce1
Rondinele <rondineleg@edu.univali.br>
2019-11-12 11:36:53 -0300

    Criando projeto Value Ref

---
+++ ReviuNetCore/ReviuNetCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReviuNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = "Rondinele";
+            var idade = 27;
+            Console.WriteLine("Criando Projetos NEtCore por : " + nome);
+        }
+    }
+}
---
+++ ReviuNetCore/ValueRefCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ValueRefCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = "Rondinele";
+            var idade = 27;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
