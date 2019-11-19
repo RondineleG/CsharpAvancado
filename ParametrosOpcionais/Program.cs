c9878f45f3ae6def810df15c8e916a6695e0aa04
Rondinele <rondineleg@edu.univali.br>
2019-11-19 11:43:29 -0300

    Usando Parametros opcionais

---
+++ ParametrosOpcionais/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ParametrosOpcionais
+{
+    public class CaixaDialogo
+    {
+        public void Show(
+            string Mensagem,
+            bool Ok, bool No,
+            //bool Cancel = false,
+            int delay = 10,
+            int tamanho = 200  // Parametros opcionais deve ser  colocados por ultimo pra evitar ambiguidade
+            )
+        {
+            Console.WriteLine(Mensagem);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dialogo1 = new CaixaDialogo();
+            dialogo1.Show(Mensagem:"Cofirmarr ?",Ok:true, No:true);
+
+            var dialogo2 = new CaixaDialogo();
+            dialogo2.Show(Mensagem: "Excluir ?", Ok: true, No: true, tamanho:20);
+
+            Console.ReadLine();
+        }
+    }
+}
