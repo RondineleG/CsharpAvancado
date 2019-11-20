8a0bf84ec40a1fe9fba2162d4ad72df5556931cb
Rondinele <rondineleg@edu.univali.br>
2019-11-20 10:34:09 -0300

    Testando tipos e repetindo codigo

---
+++ SobrecargaMetodosOverride/Arquivos.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+
+namespace SobrecargaMetodosOverride
+{
+   public class Musica : Object
+    {
+        public string Nome;
+
+        public int Tamanho;
+
+        public void Abrir()
+        {
+          Console.WriteLine("Tocando Musica :" + Nome);
+        }
+    }
+
+    public class DocumentoWord : Object
+    {
+        public string Nome;
+
+        public int Tamanho;
+
+        public void Abrir()
+        {
+            Console.WriteLine("Mostrando Documento :  " + Nome);
+        }
+    }
+
+    public class Imagen : Object
+    {
+        public string Nome;
+
+        public int Tamanho;
+
+        public void Abrir()
+        {
+            Console.WriteLine("Mostrando Imagem : " + Nome);
+        }
+    }
+
+    public class Windows
+    {
+        private ArrayList _arquivos;
+
+        public Windows()
+        {
+            this._arquivos = new ArrayList();
+        }
+
+        public void Add(object arquivo)
+        {
+            this._arquivos.Add(arquivo);
+        }
+
+        public void Abrir(object arquivo)
+        {
+            if(arquivo is Musica)
+            (arquivo as Musica).Abrir();
+
+            if (arquivo is DocumentoWord)
+                (arquivo as DocumentoWord).Abrir();
+
+            if (arquivo is Imagen)
+                (arquivo as Imagen).Abrir();
+        }
+    }
+}
--- SobrecargaMetodosOverride/Program.cs
+++ SobrecargaMetodosOverride/Program.cs
@@ -6,7 +6,19 @@ namespace SobrecargaMetodosOverride
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var mp3 = new Musica() { Nome = "Ola" };
+            var doc = new DocumentoWord() { Nome = "Mundo" };
+            var image = new Imagen() { Nome = "Dia" };
+
+            var windows = new Windows();
+            windows.Add(mp3);
+            windows.Add(doc);
+            windows.Add(image);
+
+            windows.Abrir(mp3);
+            windows.Abrir(doc);
+            windows.Abrir(image);
         }
+
     }
 }
