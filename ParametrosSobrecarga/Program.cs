38b04b26a8209a6189c5e8d595496e171a48080c
Rondinele <rondineleg@edu.univali.br>
2019-11-20 09:35:14 -0300

    Sobrecarga de metodos

---
+++ ParametrosSobrecarga/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ParametrosSobrecarga
+{
+    public class CaixaDialogo
+    {
+        //Default
+        public void Show(string Mensagen)
+        {
+            Console.WriteLine(Mensagen);
+        }
+
+        public void Show(string Mensagen, int Delay = 10)
+        {
+            Show(Mensagen);
+        }
+        public void Show(string Mensagen, Double Delay, bool Ok, bool No)
+        {
+            Show(Mensagen);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dialog1 = new CaixaDialogo();
+            dialog1.Show("Confirmar");
+
+            var dialog2 = new CaixaDialogo();
+            dialog2.Show("Excluir", 10);
+            var dialog3 = new CaixaDialogo();
+            dialog2.Show("Tem certeza ", Delay: 20, Ok:true, No: true);
+            Console.ReadLine();
+        }
+    }
+}
